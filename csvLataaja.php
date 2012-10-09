5209906446bd549d5856881d2ae0e653a0e571c9
Jussi Tiira <jussi.tiira@helsinki.fi>
2012-10-09 15:29:29 +0300

    jotain toimii!

--- csvLataaja.php
+++ csvLataaja.php
@@ -2,5 +2,5 @@
     header('Content-type: text/plain');
     header('Content-Disposition: attachment; filename="output.txt"');
     
-    readfile('output.txt')
+    readfile('output.csv')
 ?>
--- csvgen.php
+++ csvgen.php
@@ -2,17 +2,25 @@
 
 //Yhteyden muodostus
 try {
-    $yhteys = new PDO("pgsql:host=localhost;dbname=jussitii", "jussitii", "SALASANA");
+    $yhteys = new PDO("pgsql:host=localhost;dbname=jussitii", "jussitii", "f2da6f1d197719bb");
 } catch (PDOException $e) {
     die("VIRHE: " . $e->getMessage());
 }
 $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$statement="SELECT * FROM kide";
+$statement="SELECT id,c5nn FROM kide WHERE id='0125-043149_564_001'";
 
 //kyselyn suoritus
 $kysely = $yhteys->prepare($statement);
 $kysely->execute();
+
+$file = fopen("output.csv", "w");
+
+while ($rivi = $kysely->fetch()) {
+    var_dump($rivi);
+    fputcsv($file, $rivi);
+}
+fclose($file);
 
 
 
