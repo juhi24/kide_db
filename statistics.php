feaaa0bc134c1c5c97af2b46aa44eaddaacd5b31
Jussi Tiira <jussi.tiira@helsinki.fi>
2013-02-14 00:22:56 +0200

    implemented statistics filter

--- statistics.php
+++ statistics.php
@@ -5,39 +5,47 @@ $yhteys = yhdista();
 
 $method = 'c5nn';
 
-//SQL for counting particles
-foreach ($classarr as $class) {
-    $count_match .= SQLmatch_count($method, $class[0]) . " AS $class[0], ";
-    $count_matchrelat .= 'ROUND(' . SQLmatch_count($method, $class[0]) . '/NULLIF(' . SQLparticle_count($method, $class[0]) . ",0)::numeric*100,2) AS $class[0], ";
-    $count_pca .= SQLparticle_count($method, $class[0]) . " AS $class[0], ";
-    $count_user1 .= SQLparticle_count('class1', $class[0]) . " AS $class[0], ";
-    $count_user2 .= SQLparticle_count('class2', $class[0]) . " AS $class[0], ";
-}
+if (isset($_GET['results'])) {
+    //SQL for counting particles
+    foreach ($classarr as $class) {
+        $count_match .= SQLmatch_count($method, $class[0]) . " AS $class[0], ";
+        $count_matchrelat .= 'ROUND(' . SQLmatch_count($method, $class[0]) . '/NULLIF(' . SQLparticle_count($method, $class[0]) . ",0)::numeric*100,2) AS $class[0], ";
+        $count_pca .= SQLparticle_count($method, $class[0]) . " AS $class[0], ";
+        $count_user1 .= SQLparticle_count('class1', $class[0]) . " AS $class[0], ";
+        $count_user2 .= SQLparticle_count('class2', $class[0]) . " AS $class[0], ";
+    }
 
-//SELECT
-$select_userclass1 = "SELECT 2 AS row_order, 'user primary classification', $count_user1 COUNT(class1) AS total";
-$select_userclass2 = "SELECT 3 AS row_order, 'user secondary classification', $count_user2 COUNT(class2) AS total";
-$select_matchedclass = "SELECT 4 AS row_order, 'IC-PCA matches with user primary or secondary', $count_match COUNT(NULLIF($method=class1 OR $method=class2,FALSE)) AS total";
-$select_matchedrelat = "SELECT 5 AS row_order, 'matched %', $count_matchrelat ROUND(COUNT(NULLIF($method=class1 OR $method=class2,FALSE))/COUNT($method)::numeric*100,2) AS total";
-$select_pcaclass = "SELECT 1 AS row_order, 'IC-PCA classification', $count_pca COUNT($method) AS total";
+    //SELECT
+    $select_userclass1 = "SELECT 2 AS row_order, 'user primary classification', $count_user1 COUNT(class1) AS total";
+    $select_userclass2 = "SELECT 3 AS row_order, 'user secondary classification', $count_user2 COUNT(class2) AS total";
+    $select_matchedclass = "SELECT 4 AS row_order, 'IC-PCA matches with user primary or secondary', $count_match COUNT(NULLIF($method=class1 OR $method=class2,FALSE)) AS total";
+    $select_matchedrelat = "SELECT 5 AS row_order, 'matched %', $count_matchrelat ROUND(COUNT(NULLIF($method=class1 OR $method=class2,FALSE))/COUNT($method)::numeric*100,2) AS total";
+    $select_pcaclass = "SELECT 1 AS row_order, 'IC-PCA classification', $count_pca COUNT($method) AS total";
 
-//FROM
-$from_dataset = "FROM (SELECT id, $method FROM kide) AS pca
-RIGHT JOIN (SELECT kide_id, class1, class2 FROM man_class WHERE classified_by='{$_SESSION["valid_user"]}') AS usr
-ON pca.id=usr.kide_id";
+    //FROM
+    $from_dataset = "FROM (SELECT id, time, $method, dmax, ar, asprat FROM kide) AS pca
+    RIGHT JOIN (SELECT kide_id, class1, class2 FROM man_class WHERE classified_by='{$_SESSION["valid_user"]}') AS usr
+    ON pca.id=usr.kide_id
+    WHERE dmax BETWEEN :sizemin AND :sizemax
+    AND ar BETWEEN :armin AND :armax
+    AND asprat BETWEEN :aspratmin AND :aspratmax
+    AND time BETWEEN :datestart AND :dateend";
 
-//prepare and execute query
-try {
-    $kysely = $yhteys->prepare("SELECT * FROM ($select_pcaclass $from_dataset 
+    //prepare and execute query
+    try {
+        $kysely = $yhteys->prepare("SELECT * FROM ($select_pcaclass $from_dataset 
         UNION $select_matchedclass $from_dataset 
             UNION $select_userclass1 $from_dataset 
                 UNION $select_userclass2 $from_dataset 
                     UNION $select_matchedrelat $from_dataset) AS A ORDER BY row_order");
-    $kysely->execute();
-} catch (PDOException $e) {
-    pdo_error($e);
+        $kysely->execute(array(':sizemin' => $_POST['size_min'], ':sizemax' => $_POST['size_max'],
+            ':datestart' => $default['datestart'], ':dateend' => $default['dateend'], 'armin' => $_POST['ar_min'],
+            ':armax' => $_POST['ar_max'], ':aspratmin' => $_POST['asprat_min'], ':aspratmax' => $_POST['asprat_max']));
+    } catch (PDOException $e) {
+        pdo_error($e);
+    }
+    $stat_array = $kysely->fetchAll(PDO::FETCH_ASSOC);
 }
-$stat_array = $kysely->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -52,22 +60,36 @@ $stat_array = $kysely->fetchAll(PDO::FETCH_ASSOC);
     <body>
         <?php require_once 'apu/header.html'; ?>
         <h2>Statistics</h2>
-        <h3>Manual classification results</h3>
-        <table class="hide1">
-            <?php
+        <h3>Stat filters</h3>
+        <form name="getstats" method="post" action="statistics.php?results">
+            <fieldset><legend>Particle properties</legend>
+                <?php
+                echo HTMLdmax($default['sizemin'], $default['sizemax']);
+                echo HTMLar($default['armin'], $default['armax']);
+                echo HTMLasprat($default['aspratmin'], $default['aspratmax']);
+                ?>
+            </fieldset>
+            <input type="submit" name="submit" value="Generate statistics">
+        </form>
+        <?php
+        if (isset($_GET['results'])) {
+            echo '<h3>Manual classification results</h3>';
+            echo '<table class="hide1">';
+
             echo '<tr><td></td><td></td>';
             foreach ($classarr as $class) {
                 echo "<td>$class[1]</td>";
             }
             echo '<td>Total</td></tr>';
-            foreach ($stat_array as $rows => $row) {
+            foreach ($stat_array as $row) {
                 echo '<tr>';
-                foreach ($row as $col => $cell) {
+                foreach ($row as $cell) {
                     echo "<td>$cell</td>";
                 }
                 echo '</tr>';
             }
-            ?>
-        </table>
+            echo '</table>';
+        }
+        ?>
     </body>
 </html>
